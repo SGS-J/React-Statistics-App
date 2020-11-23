57378cb8174bd69ee9650811a6ca9ba233604c3f
SGS-J <santigarsan07@gmail.com>
2020-11-23 13:25:51 -0500

    Created the instructions window

--- src/components/DataInsert.js
+++ src/components/DataInsert.js
@@ -1,47 +1,49 @@
 import React from 'react';
 import DataValue from './DataValue';
+import HelpWindow from './HelpWindow';
 import './DataInsert.css';
 
 export default function DataInsert(props) {
-  const data = props.data.slice();
-  const elements = data.map(val => {
-    return (
-      <DataValue
-        key={val._id}
-        id={val._id}
-        dataFreq={val.dataFreq}
-        dataValue={val.dataValue}
-        onInsert={props.onInsert}
-      />
-    );
-  });
+    const data = props.data.slice();
+    const elements = data.map(val => {
+      return (
+        <DataValue
+          key={val._id}
+          id={val._id}
+          dataFreq={val.dataFreq}
+          dataValue={val.dataValue}
+          onInsert={props.onInsert}
+        />
+      );
+    });
 
-  return (
-    <section id="data-insert">
-      <button className="btn btn-help shadow-none">?</button>
-      <table className="custom-table">
-        <thead className="table-head">
-          <tr>
-            <th scope="col">Frequency :</th>
-            <th scope="col">Value :</th>
-          </tr>
-        </thead>
-        <tbody className="table-elements">{elements}</tbody>
-      </table>
-      <div className="buttons">
-        <button
-          className="btn btn-outline-default shadow-none"
-          onClick={props.onClickAdd}
-        >
-          +
-        </button>
-        <button
-          className="btn btn-outline-default shadow-none"
-          onClick={props.onClickRemove}
-        >
-          -
-        </button>
-      </div>
-    </section>
-  );
+    return (
+      <section id="data-insert">
+        <button className="btn btn-help shadow-none">?</button>
+        <HelpWindow />
+        <table className="custom-table">
+          <thead className="table-head">
+            <tr>
+              <th scope="col">Frequency :</th>
+              <th scope="col">Value :</th>
+            </tr>
+          </thead>
+          <tbody className="table-elements">{elements}</tbody>
+        </table>
+        <div className="buttons">
+          <button
+            className="btn btn-outline-default shadow-none"
+            onClick={props.onClickAdd}
+          >
+            +
+          </button>
+          <button
+            className="btn btn-outline-default shadow-none"
+            onClick={props.onClickRemove}
+          >
+            -
+          </button>
+        </div>
+      </section>
+    );
 }
--- src/components/HelpWindow.js
+++ src/components/HelpWindow.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './HelpWindow.css';
 
-function HelpWindow() {
-   return (
-      <section>
-         <p>
-            This is the help.
-         </p>
-      </section>
-   )
+export default function HelpWindow() {
+  return (
+    <div id="helpWin">
+      <h5>INSTRUCTIONS:</h5>
+      <p className="">
+          {`Put the number of occurrences on frequency
+            inputs below and their respective value in
+            the sample as numbers(e.g. Animals: {Dog: 1, Cat: 2, etc}.)
+            Use the arrows below table value to add or remove the values.`}
+      </p>
+    </div>
+  );
 }
